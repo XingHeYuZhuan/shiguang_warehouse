4db9e84072a2f241f2d9274649a5aca146c35822
Haooz <133077949+HaoZai000@users.noreply.github.com>
2026-08-31 19:48:35 +0800

    Update fit.js

--- resources/FIT/fit.js
+++ resources/FIT/fit.js
@@ -393,34 +393,6 @@ async function saveCourses(parsedCourses) {
     }
 }
 
-const TimeSlots = [
-    { number: 1, startTime: "08:00", endTime: "08:45" },
-    { number: 2, startTime: "08:55", endTime: "09:40" },
-    { number: 3, startTime: "10:05", endTime: "10:50" },
-    { number: 4, startTime: "11:00", endTime: "11:45" },
-    { number: 5, startTime: "13:30", endTime: "14:15" },
-    { number: 6, startTime: "14:25", endTime: "15:10" },
-    { number: 7, startTime: "15:20", endTime: "16:05" },
-    { number: 8, startTime: "16:05", endTime: "16:50" },
-    { number: 9, startTime: "18:00", endTime: "18:45" },
-    { number: 10, startTime: "18:45", endTime: "19:30" },
-    { number: 11, startTime: "19:30", endTime: "20:15" }
-];
-
-async function importPresetTimeSlots(timeSlots) {
-    if (timeSlots.length === 0) {
-        window.shiguangBridge.showToast("警告：时间段为空，未导入时间段信息。");
-        return;
-    }
-
-    try {
-        await window.shiguangBridgePromise.savePresetTimeSlots(JSON.stringify(timeSlots));
-        window.shiguangBridge.showToast("预设时间段导入成功！");
-    } catch (error) {
-        window.shiguangBridge.showToast("导入时间段失败: " + error.message);
-    }
-}
-
 async function runImportFlow() {
     const alertConfirmed = await promptUserToStart();
     if (!alertConfirmed) {
@@ -458,8 +430,6 @@ async function runImportFlow() {
     } catch (error) {
         window.shiguangBridge.showToast(`课表配置保存失败: ${error.message}`);
     }
-
-    await importPresetTimeSlots(TimeSlots);
 
     window.shiguangBridge.showToast(`课程导入成功，共导入 ${courses.length} 门课程！`);
     window.shiguangBridge.notifyTaskCompletion();
